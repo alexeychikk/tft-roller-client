587ed111bdfd1b2b0a840cfae1d4fda9b39049a2
Alex Zinkevych <alexdarkmm@gmail.com>
2024-01-11 00:54:59 +0200

    feat: build action

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react(), svgr()],
+  base: '',
+  build: { outDir: 'docs' },
 });
