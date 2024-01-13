8ce13ceb41dcb12902f0f8b2f9d786b935b99fbd
Alex Zinkevych <alexdarkmm@gmail.com>
2024-01-13 20:51:02 +0200

    fix: reroll cost text + minor styles improvements

--- src/components/dumb/ShopButton/ShopButtonReroll.tsx
+++ src/components/dumb/ShopButton/ShopButtonReroll.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 
 import { ReactComponent as IconReroll } from '@src/assets/icons/common/reroll.svg';
-import { GOLD_PER_EXPERIENCE_BUY } from '@src/constants';
+import { GOLD_PER_REROLL } from '@src/constants';
 
 import { ShopButton, ShopButtonExtendedProps } from './ShopButton';
 import styles from './ShopButton.module.scss';
@@ -14,7 +14,7 @@ export const ShopButtonReroll: React.FC<ShopButtonExtendedProps> = ({
   return (
     <ShopButton
       className={clsx(styles.reroll, className)}
-      cost={GOLD_PER_EXPERIENCE_BUY}
+      cost={GOLD_PER_REROLL}
       icon={IconReroll}
       name="Reroll"
       {...restProps}
